4ee578646c80ff3d7b099446074848ec93dc09d9
encode-jane12 <admin@stanfordinstitute.in>
2026-09-26 12:40:36 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,9 @@ import Home from '@/src/views/Home/Home';
 export const metadata = {
   title: 'Home | SNS Construction',
   description: 'Learn more about Home at SNS Construction.',
+  alternates: {
+    canonical: '/',
+  },
 };
 
 export default function Page() {
